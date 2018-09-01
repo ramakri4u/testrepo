074475762e404690b78d4fc34c4b287a9d48d7f1
rama <rkmadire@gmail.com>
2018-09-01 19:55:10 +0530

    Commit modification

--- testrepo/src/testrepo/Test.java
+++ testrepo/src/testrepo/Test.java
@@ -3,7 +3,7 @@ package testrepo;
 public class Test {
 
 	public static void main(String[] args) {
-		System.out.print("Hello Rama, Welcome to java Prg");
+		System.out.print("Hello Rama, Welcome to java Program");
 
 	}
 
